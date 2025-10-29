edf96fc5d3e6708f592c06c2d2bb85f2b4806ca4
Pedro Miguel <pm987162@gmail.com>
2025-10-29 00:04:33 -0400

    minor fixex: still configuring user navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import { Route, NavLink } from "react-router-dom";
 import {Logout} from "../services/logout";
 
-
 function Header() {
+
+  const loginUser = localStorage.getItem("usuario");
+
   return (
-    <header className="flex justify-between align-center p-4 bg-blue-700 text-white">
+    <header className="flex justify-between items-center align-center p-4 bg-blue-700 text-white">
       <NavLink to="/">Home</NavLink>
-      <div className="flex gap-4">
-        <NavLink to="/todo">Todo</NavLink>
-        <NavLink to="/register">Register</NavLink>
-        <button onClick={Logout} className="px-4 py-2 bg-white text-blue-700 rounded">
+      <div className="flex items-center gap-4">
+        {loginUser && (<NavLink to="/todo">Todo</NavLink>)}
+        {!loginUser && (<NavLink to="/register" >Register</NavLink>)}
+        {loginUser && (<button onClick={Logout} className="px-4 py-2 bg-white text-blue-700 rounded">
       Logout
-    </button>
+    </button>)}
       </div>
     </header>
   );
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -14,7 +14,7 @@ export default function Register() {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-        setForm(prev => ({ ...prev, [name]: value }));
+    setForm(prev => ({ ...prev, [name]: value }));
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -37,8 +37,8 @@ export default function Register() {
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                   <div>
-                      <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="text" name="nome" id="nome" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"/>
+                      <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                      <input type="text" name="nome" id="nome" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nickname"/>
                   </div>
                   <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
