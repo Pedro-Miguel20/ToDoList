8a099e2794e3d5fbccb311033c1489e4b42bb80c
Pedro-Miguel20 <pm987162@gmail.com>
2025-11-19 08:46:29 -0400

    Feature: development for merging with master

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
-  ],
-  theme: {
-    extend: {
-      rotate: {
-        "15": "15deg",
-        "30": "30deg",
-        "45": "45deg",
-      },
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+];
+export const theme = {
+  extend: {
+    rotate: {
+      "15": "15deg",
+      "30": "30deg",
+      "45": "45deg",
     },
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+};
+export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
